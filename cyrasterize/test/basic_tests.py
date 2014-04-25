4c565f308fb0499830a6a5ec7a62781f07ebcde7
Patrick Snape <patricksnape@gmail.com>
2014-04-25 15:44:17 +0000

    Add a basic test
    Rasterize a simple image as a test

---
+++ cyrasterize/test/basic_tests.py
@@ -0,0 +1,16 @@
+import numpy as np
+from cyrasterize import CyRasterizer
+from numpy.testing import assert_allclose
+
+
+def test_basic_random():
+    c = CyRasterizer(width=100, height=100)
+
+    points = np.array([[-1, -1, 0], [1, -1, 0], [1, 1, 0], [-1, 1, 0]])
+    trilist = np.array([[0, 1, 2], [2, 3, 0]])
+    colours = np.random.uniform(size=(100, 100, 3))
+    tcoords = np.array([[0, 0], [1, 0], [1, 1], [0, 1]])
+
+    rgb_image, float_image, mask = c.rasterize(points, trilist, colours, tcoords)
+    
+    assert_allclose(rgb_image, colours)
