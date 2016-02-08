113fe3fe954d96dc702ca37e60e4e8fbd569f2d3
James Booth <jabooth@gmail.com>
2016-02-08 14:43:51 +0000

    fixes for running tests with conda

--- cyrasterize/tests.py
+++ cyrasterize/tests.py
@@ -1,12 +1,13 @@
 import numpy as np
 import os
 import sys
-from cyrasterize import CyRasterizer, ShaderSource
+from cyrasterize import CyRasterizer
 from numpy.testing import assert_allclose
 
 SHADER_BASEPATH = os.path.join(os.path.dirname(sys.modules['cyrasterize'].__file__), 'shaders', 'texture_shader')
 DEFAULT_VERTEX_SHADER_SRC = open(SHADER_BASEPATH + '.vert', 'rb').read()
 DEFAULT_FRAGMENT_SHADER_SRC = open(SHADER_BASEPATH + '.frag', 'rb').read()
+
 
 def test_basic_random():
     c = CyRasterizer(width=100, height=100)
@@ -20,8 +21,5 @@ def test_basic_random():
     tcoords = np.array([[0, 0], [1, 0], [1, 1], [0, 1]])
 
     rgb_image, float_image, mask = c.rasterize(points, trilist, colours, tcoords)
-    
-    assert_allclose(rgb_image, colours)
 
-def test_load_empty_shader():
-    shader = ShaderSource('', 'fragment')
+    assert_allclose(rgb_image, colours)
