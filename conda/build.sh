771acd1e6c814f24ecd62cd82066ae68e3767926
Patrick Snape <patricksnape@gmail.com>
2016-05-12 14:40:34 +0200

    Fixing conda building

--- conda/build.sh
+++ conda/build.sh
@@ -2,14 +2,7 @@
 
 cp -r $RECIPE_DIR/../ .
 
-case "$OSTYPE" in
-  darwin*)  export CXX=clang++;; 
-  linux*)   export CFLAGS="$CFLAGS -m${ARCH}";export LDLAGS="$LDLAGS -m${ARCH}";;
-  *)        echo "WARNING: Unknown OS - ${OSTYPE}";;
-esac
-
 export CFLAGS="-I$PREFIX/include $CFLAGS"
 export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
 
 "$PYTHON" setup.py install --single-version-externally-managed --record=/tmp/record.txt
-
