34c8d40ea200b482a798f7b5cb88220825705da4
Patrick Snape <patricksnape@gmail.com>
2016-05-12 15:19:57 +0200

    Run tests on travis

--- conda/run_test.py
+++ conda/run_test.py
@@ -2,8 +2,4 @@ import os
 from cyrasterize.tests import test_basic_random
 
 if __name__ == "__main__":
-    if (os.environ.get('IN_VM') is None and
-        os.environ.get('TRAVIS') is None and
-        os.environ.get('APPVEYOR') is None and
-        os.environ.get('CI') is None):
-        test_basic_random()
+    test_basic_random()
