853f2907cec7e40c97b89a2456cb0ef123005373
James Booth <jabooth@gmail.com>
2016-08-05 07:09:06 +0100

    fix test blocking for jenkins

--- conda/run_test.py
+++ conda/run_test.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     if (os.environ.get('IN_VM') is None and
         os.environ.get('TRAVIS') is None and
         os.environ.get('APPVEYOR') is None and
-        os.environ.get('CI') is None) and
-        'JENKINS_URL' not in os.environ:
+        os.environ.get('CI') is None and
+        'JENKINS_URL' not in os.environ):
         test_basic_random()
